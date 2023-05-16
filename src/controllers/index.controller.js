bc3f43bf0bdd7f13430b1f88ed1542dd912736d9
Javier Sierra <javiersierrasoftware@gmail.com>
2023-05-15 20:35:37 -0500

    Actualización

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,16 @@
+
+
+const indexCtrl = {};
+
+indexCtrl.renderIndex = (req, res) => {
+  res.render('index');
+};
+
+indexCtrl.renderAbout = (req, res) => {
+  res.render('about');
+};
+
+indexCtrl.renderHome = (req, res) => {
+  res.render('home');
+};
+module.exports = indexCtrl;
